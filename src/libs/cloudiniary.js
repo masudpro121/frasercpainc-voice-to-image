50acfa7dec48e34f79d3d766fb0ab6c198560f96
masudpro121 <masud.pro.121@gmail.com>
2023-08-01 11:28:12 +0600

    image upload done

---
+++ src/libs/cloudiniary.js
@@ -0,0 +1,24 @@
+import {v2 as cloudinary} from 'cloudinary';
+require('dotenv').config()         
+
+
+
+let isConnected = false;
+async function cloudinaryConnect () {
+  return new Promise((resolve, reject)=>{
+    if(!isConnected){
+      console.log('connecting cloudinary');
+       cloudinary.config({ 
+        cloud_name: 'dlwfpkwwp', 
+        api_key: '634977678565211', 
+        api_secret:  process.env.CLOUDINARY_SECRET
+      });
+      resolve('Connected')
+    }else{
+      console.log('already connected cloudinary');
+      resolve('Connected')
+    }
+  })
+}
+
+export default cloudinaryConnect
--- src/pages/api/generate-image.js
+++ src/pages/api/generate-image.js
@@ -1,5 +1,7 @@
 import { STABLEDIFFUSION_KEY } from "@/configs";
-
+import cloudinaryConnect from "@/libs/cloudiniary";
+import {v2 as cloudinary} from 'cloudinary';
+const {v4:uuid} = require('uuid')
 
 require("dotenv").config();
 const axios = require("axios");
@@ -7,9 +9,20 @@ const axios = require("axios");
 export default async function handler(req, res) {
   if (req.method == "POST") {
     const { sample, dimension, prompt, negativePrompt, model } = req.body;
+    
+    
     generate({ sample, dimension, prompt, negativePrompt, model })
       .then(async (result) => {
-        res.json(result);
+        const uploads = result.output.map((img, i)=>uploadImage(img,`${i} - ${prompt.slice(0,20)} - ${uuid()}`))
+        Promise.all(uploads)
+        .then(values=>{
+          console.log(values, 'values');
+          res.json(result);
+        })
+        .catch(err=>{
+          res.json({ status: "something wrong" });
+          console.log(err);
+        })
       })
       .catch((err) => {
         console.log(err);
@@ -18,8 +31,26 @@ export default async function handler(req, res) {
   }
 }
 
+const uploadImage = async (image, text) => {
+  await cloudinaryConnect();
+  return new Promise((resolve, reject)=>{
+    cloudinary.uploader.upload(
+      image,
+      { folder: "image", public_id: text },
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        }
+        return resolve(result.url)
+      }
+    );
+ })
+};
+
 const generate = ({ sample, dimension, prompt, negativePrompt, model }) => {
-  const defaultNegative = negativePrompt? negativePrompt : "naked, porn, nudity, sex, adult, boobs, pussy, nude,  skimpy clothes, sexy, sexualized"
+  const defaultNegative = negativePrompt
+    ? negativePrompt
+    : "naked, porn, nudity, sex, adult, boobs, pussy, nude,  skimpy clothes, sexy, sexualized";
 
   let imageConfig = {
     key: STABLEDIFFUSION_KEY,
@@ -38,41 +69,41 @@ const generate = ({ sample, dimension, prompt, negativePrompt, model }) => {
     embeddings_model: "embeddings_model_id",
     webhook: null,
     track_id: null,
-  }
-  let apiUrl="https://stablediffusionapi.com/api/v3/text2img"
+  };
+  let apiUrl = "https://stablediffusionapi.com/api/v3/text2img";
 
-  if(model){
+  if (model) {
     imageConfig = {
-      "key": STABLEDIFFUSION_KEY,
-      "model_id": model,
-      "prompt": prompt,
-      "negative_prompt": defaultNegative,
-      "samples": sample,
-      "num_inference_steps": "30",
-      "safety_checker": "no",
-      "enhance_prompt": "yes",
-      "seed": null,
-      "guidance_scale": 7.5,
-      "multi_lingual": "no",
-      "panorama": "no",
-      "self_attention": "no",
-      "upscale": "no",
-      "embeddings_model": null,
-      "lora_model": null,
-      "tomesd": "yes",
-      "clip_skip": "2",
-      "use_karras_sigmas": "yes",
-      "vae": null,
-      "lora_strength": null,
-      "scheduler": "UniPCMultistepScheduler",
-      "webhook": null,
-      "track_id": null
-    }
-    apiUrl = "https://stablediffusionapi.com/api/v4/dreambooth"
+      key: STABLEDIFFUSION_KEY,
+      model_id: model,
+      prompt: prompt,
+      negative_prompt: defaultNegative,
+      samples: sample,
+      num_inference_steps: "30",
+      safety_checker: "no",
+      enhance_prompt: "yes",
+      seed: null,
+      guidance_scale: 7.5,
+      multi_lingual: "no",
+      panorama: "no",
+      self_attention: "no",
+      upscale: "no",
+      embeddings_model: null,
+      lora_model: null,
+      tomesd: "yes",
+      clip_skip: "2",
+      use_karras_sigmas: "yes",
+      vae: null,
+      lora_strength: null,
+      scheduler: "UniPCMultistepScheduler",
+      webhook: null,
+      track_id: null,
+    };
+    apiUrl = "https://stablediffusionapi.com/api/v4/dreambooth";
   }
-  if(dimension){
-    imageConfig.width= dimension.width
-    imageConfig.height= dimension.height
+  if (dimension) {
+    imageConfig.width = dimension.width;
+    imageConfig.height = dimension.height;
   }
   console.log(imageConfig);
   return new Promise((resolve, reject) => {
